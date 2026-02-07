b357c21893264e8a3b6360d32195b644f3cc6168
ytz <1472290813@qq.com>
2026-02-07 10:15:56 +0800

    feat: 修复了小罗消失动画错误的bug

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,7 @@ import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+// 生成 RSS feed，包含所有博客文章
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
