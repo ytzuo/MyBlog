5b986e8e6fa982a31176af786095e1df95d6663e
ytz <1472290813@qq.com>
2026-02-10 12:17:19 +0800

    feat: 调整了目录

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://saten.website',
 	integrations: [mdx(), sitemap()],
 	prefetch: true,
 });
