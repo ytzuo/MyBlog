784a305e2b852aa1a421d26114faa466934d13f8
ytz <1472290813@qq.com>
2026-04-21 11:17:19 +0800

    feat: 增加对于转载博文的支持

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,24 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.string(),
-			updatedDate: z.string().optional(),
-			heroImage: image().optional(),
-			heroImageScale: z.number().optional(),
-			tags: z.array(z.string()).optional(),
-		}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.string(),
+      updatedDate: z.string().optional(),
+      heroImage: image().optional(),
+      heroImageScale: z.number().optional(),
+      tags: z.array(z.string()).optional(),
+      reprintAuthor: z.string().optional(),
+      reprintUrl: z.string().optional(),
+      reprintTitle: z.string().optional(),
+    }),
 });
 
 export const collections = { blog };
