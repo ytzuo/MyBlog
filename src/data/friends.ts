a6f131271fc2d9f3fce628395cd0543423b10da4
satenyang <satenyang@outlook.com>
2026-07-12 11:24:21 +0800

    feat: 添加友情链接

---
+++ src/data/friends.ts
@@ -0,0 +1,19 @@
+import elecmonkeyAvatar from "../assets/elecmonkey.png";
+
+export interface FriendLink {
+    name: string;
+    titleSuffix?: string;
+    url: string;
+    description: string;
+    avatar?: ImageMetadata;
+}
+
+export const friendLinks: FriendLink[] = [
+    {
+        name: "Elecmonkey",
+        titleSuffix: "'s Garden",
+        url: "https://elecmonkey.com",
+        description: "来自 elecmonkey.com 的友情链接。",
+        avatar: elecmonkeyAvatar,
+    },
+];
