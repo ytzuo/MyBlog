a791bc71ceb0544f3d8c6d5b30cdad5481105a55
satenyang <satenyang@outlook.com>
2026-08-02 19:20:22 +0800

    feat: 英文404

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://saten.website',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap({ filter: (page) => !page.endsWith('/en/404/') }),
+	],
 	prefetch: true,
 	compressHTML: true,
 });
